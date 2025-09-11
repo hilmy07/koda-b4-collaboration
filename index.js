25c901c11b9d137b60de0f9c6bcd8a099a14f123
fulsep <57238803+fulsep@users.noreply.github.com>
2025-09-11 16:14:38 +0700

    feat: init project

---
+++ index.js
@@ -0,0 +1,34 @@
+import { input } from "./lib/input.js";
+import { showAsMenu } from "./lib/output.js";
+import { selectProduct } from "./lib/products.js";
+import { showCarts } from "./lib/carts.js";
+
+const listMenu = [
+  {
+    title: "Pilih product",
+    func: selectProduct,
+  },
+  {
+    title: "Keranjang",
+    func: showCarts
+  }
+];
+
+async function main(){
+  let attach = true;
+  while(attach){
+    showAsMenu(listMenu);
+    console.log("\n\n0. Keluar");
+    const select = (await input("Pilih menu: "));
+    if(select === -1){
+      attach = false;
+      continue;
+    }
+
+    if(typeof listMenu[select]?.func === "function"){
+      await listMenu[select].func();
+    }
+  }
+}
+
+main();
